289a64f32e68109e7b10a68955caeee6b0e3c775
lidiaAsamnew <lidia.yirbe@gmail.com>
2025-06-18 19:29:38 +0300

    fix: update auth hook

--- Frontend/hooks/use-auth.tsx
+++ Frontend/hooks/use-auth.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type React from "react";
+import * as React from "react";
 import { createContext, useContext, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import type { User } from "@/types/user";
